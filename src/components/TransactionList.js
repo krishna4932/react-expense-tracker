cf93996e8b8248697dbc4187b0e960cd65c75f3f
Krishna Undamatla <krishna.undamatla@khumbusystems.com>
2022-03-09 16:15:24 +0530

    formatting

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,7 +1,6 @@
-import React, { useContext } from 'react';
-import { Transaction } from './Transaction';
-
-import { GlobalContext } from '../context/GlobalState';
+import React, { useContext } from "react";
+import { Transaction } from "./Transaction";
+import { GlobalContext } from "../context/GlobalState";
 
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
@@ -10,8 +9,10 @@ export const TransactionList = () => {
     <>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </>
-  )
-}
+  );
+};
